06dd8aed304d842ecea823322d1307537bb14cb0
Daniel Li <dli40@calpoly.edu>
2019-02-01 22:20:19 -0800

    lmao
    commiting from local

---
+++ Assets/test.cs.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test.cs : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       //HELLO WORLD! DID THIS PUSH TO MASTER OR AM I LITERALLY MASTER
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
